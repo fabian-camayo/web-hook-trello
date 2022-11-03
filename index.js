9e13f9da1792ca4f05a24b0ba699bf2d4acdfd13
Fabian Camayo <52054754+fabian-camayo@users.noreply.github.com>
2022-11-03 14:56:56 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
+const cors = require('cors')
 var bodyParser = require("body-parser");
+app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 

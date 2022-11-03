255ac33761fc9ea6ad68f3772775da54368b9d61
Fabian Camayo <52054754+fabian-camayo@users.noreply.github.com>
2022-11-03 14:29:58 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const app = express();
+var bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 app.use(express.static(__dirname + '/public/'));
-app.post(‘/api/trello/task_support_notification’,(request,response) => {
-console.log(request.body);
+
+app.post("/api/trello/task_support_notification", (req, res, next) => {
+   console.log(req);
 });
 app.listen(process.env.PORT || 3000, function() {
   console.log('Servidor web escuchando en el puerto');
